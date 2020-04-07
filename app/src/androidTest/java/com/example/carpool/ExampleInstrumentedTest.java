bd111a14f0f5c6ae526d3ca5d2c2f234c77af4ca
Kunj Jagdishbhai Tandel <Ktandel9021@conestogac.on.ca>
2020-04-07 14:40:54 -0400

    UI test cases and unit test cases changes

--- app/src/androidTest/java/com/example/carpool/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/carpool/ExampleInstrumentedTest.java
@@ -2,12 +2,20 @@ package com.example.carpool;
 
 import android.content.Context;
 
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,12 +24,26 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    @Rule
+    public ActivityTestRule<activity_logIn> activityRule =
+            new ActivityTestRule<>(activity_logIn.class);
 
-        assertEquals("com.example.carpool", appContext.getPackageName());
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withId(R.id.txt_username_logIn));
+        onView(withId(R.id.txt_password_logIn));
+
+
+    }
+
+    @Test
+    public void listGoesTheFold() {
+        onView(withId(R.id.btn_login));
+        onView(withId(R.id.progressBar));
+        onView(withId(R.id.lbl_registerNow));
+        onView(withId(R.id.txt_forget_password_login));
+
     }
 }
--- app/src/test/java/com/example/carpool/ExampleUnitTest.java
+++ app/src/test/java/com/example/carpool/ExampleUnitTest.java
@@ -10,10 +10,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void enteredData_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+   
 
     @Test
     public void login_Checked() {
